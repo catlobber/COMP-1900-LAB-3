f1537fa2a01796454feddcbf3ced61258de02a90
catlobber <themogmog360@gmail.com>
2023-10-05 06:59:42 -0500

    hi agaib

---
+++ COMP 1900 Lab 3/best_gps_ever.py
@@ -0,0 +1,37 @@
+#Get user inputs
+print("Starting Corner:")
+starting_street = int(input("Street - "))
+starting_avenue = int(input("Avenue - "))
+print("Ending Corner:")
+ending_street = int(input("Street - "))
+ending_avenue = int(input("Avenue - "))
+#Find distance needed to travel and the direction and print where to go to user
+if (starting_street > ending_street):
+    movementy = (starting_street - ending_street) * 750
+    directiony = ('South')
+    print(f'Take Avenue {starting_avenue} {directiony} for {movementy}ft until you get to Street {ending_street} ')
+elif (starting_street < ending_street):
+    movementy = (ending_street - starting_street) * 750
+    directiony = ('North')
+    print(f'Take Avenue {starting_avenue} {directiony} for {movementy}ft until you get to Street {ending_street} ')
+    
+else: 
+    movementy = 0  
+
+#Find distance, direction, and turn and print where to go to user
+if (starting_avenue > ending_avenue):
+    movementx = (starting_avenue - ending_avenue) * 750
+    turn = ('Right')
+    directionx = ('East')
+    print(f'Turn {turn} on Street {ending_street}')
+    print(f'Take Street {ending_street} {directionx} for {movementx} until you get to Avenue {ending_avenue}')
+elif (starting_street < ending_street):
+    movementx = (ending_avenue - starting_avenue) * 750
+    turn = ('Left')
+    directionx = ('West')
+    print(f'Turn {turn} on Street {ending_street}')
+    print(f'Take Street {ending_street} {directionx} for {movementx} until you get to Avenue {ending_avenue}')
+else: 
+    movementx = 0  
+#Final statement which always prints
+print("Yay! You have arrived!")
--- COMP 1900 Lab 3/tax_calculator.py
+++ COMP 1900 Lab 3/tax_calculator.py
@@ -1,42 +1,62 @@
-
+#Initialize all brackets
+first_bracket = 0
+second_bracket = 0
+third_bracket = 0
+fourth_bracket = 0
+fifth_bracket = 0
+sixth_bracket = 0
+seventh_bracket = 0
 #Obtain user income
 income = float(input("What was your 2022 income? "))
 #Tax for the first bracket
 if (income < 10275):
     first_bracket = (10275 - income) * 0.1
-    print(f"First $10,275: $ {first_bracket}")
-else:
+    print(f"First $10,275: $ {first_bracket:.2f}")
+elif(income >= 10275):
     first_bracket = 10275 * 0.1
-    print(f"First $10,275: $ {first_bracket}")
-#Tax for the second bracket
-if (income > 10275 and income < 41775):
-    second_bracket = (income - 10275) * 0.12
-    print(f"$10,275-$41775: $ {second_bracket}")
-else: 
-    second_bracket = (41775 - 10275) * 0.12
-    print(f"$10,275-$41775: $ {second_bracket}")
-#Tax for the third bracket
-if (income > 41775 and income < 89075):
-    third_bracket = (income - 41775) * 0.22
-    print(f"$41,775-$89,075: $ {second_bracket}")
-else:
-    third_bracket = (89075 - 41775) * 0.22
-    print(f"$10,275-$41775: $ {second_bracket}")
-#Tax for the fourth bracket
-if (income > 89075 and income < 170050):
-    fourth_bracket = (income - 89075) * 0.24
-else: 
-    fourth_bracket = (170050 - 89075) * 0.24
-#Tax for the fifth bracket
-if (income > 170050 and income < 215950):
-    fifth_bracket = (income - 170050) * 0.32
-else:
-    fifth_bracket = (215950 - 170050) * 0.32
-#Tax for the sixth bracket
-if (income > 215950 and income < 539900):
-    sixth_bracket = (income - 215950) * 0.35
-else: 
-    sixth_bracket = (539900 - 215950) * 0.35
-#Tax for the seventh bracket
-if (income > 539900):
-    seventh_bracket = (income - 539900)
+    print(f"First $10,275: $ {first_bracket:.2f}")
+    #Tax for second bracket
+    if (income > 10275 and income < 41775):
+     second_bracket = (income - 10275) * 0.12
+     print(f"$10,275-$41775: $ {second_bracket:.2f}")
+    elif (income >= 41775): 
+     second_bracket = (41775 - 10275) * 0.12
+     print(f"$10,275-$41775: $ {second_bracket:.2f}")
+     #Tax for third bracket
+     if (income > 41775 and income < 89075):
+      third_bracket = (income - 41775) * 0.22
+      print(f"$41,775-$89,075: $ {third_bracket:.2f}")
+     elif (income >= 89075):
+      third_bracket = (89075 - 41775) * 0.22
+      print(f"$41,775-$89,075: $ {third_bracket:.2f}")
+      #Tax for fourth bracket
+      if (income > 89075 and income < 170050):
+       fourth_bracket = (income - 89075) * 0.24
+       print(f"$89,075-$170,050: $ {fourth_bracket:.2f}")
+      elif (income >= 170050): 
+        fourth_bracket = (170050 - 89075) * 0.24
+        print(f"$89,075-$170,050: $ {fourth_bracket:.2f}")
+        #Tax for fifth bracket
+        if (income > 170050 and income < 215950):
+         fifth_bracket = (income - 170050) * 0.32
+         print(f"$170,050-$215,950: $ {fifth_bracket:.2f}")
+        elif (income >= 215950):
+         fifth_bracket = (215950 - 170050) * 0.32
+         print(f"$170,050-$215,950: $ {fifth_bracket:.2f}")
+         #Tax for sixth bracket
+         if (income > 215950 and income < 539900):
+          sixth_bracket = (income - 215950) * 0.35
+          print(f"$215,950-$539,900: $ {sixth_bracket:.2f}")
+         elif (income >= 539900): 
+          sixth_bracket = (539900 - 215950) * 0.35
+          print(f"$215,950-$539,900: $ {sixth_bracket:.2f}")
+          #Tax for seventh bracket
+          if (income > 539900):
+           seventh_bracket = (income - 539900)
+           print(f"$539,900+: $ {seventh_bracket:.2f}")
+#Total Taxes
+total_taxes = first_bracket + second_bracket + third_bracket + fourth_bracket + fifth_bracket + sixth_bracket + seventh_bracket
+#Effective tax rate
+eff_tax_rate = total_taxes/income
+print(f"Total Tax Owed: ${total_taxes:.2f}")
+print(f"Effective Tax Rate = {eff_tax_rate*100:.1f}%")
